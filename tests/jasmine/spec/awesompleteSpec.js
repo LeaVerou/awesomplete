d7fca553b9e3ef7a8bda7a3ccec8e840457a0e69
Juan Torres <juanto121@hotmail.com>
2015-03-27 23:19:13 -0500

    testing awesomplete object creation

--- tests/jasmine/spec/awesompleteSpec.js
+++ tests/jasmine/spec/awesompleteSpec.js
@@ -1,3 +1,44 @@
+describe("awesomplete",function(){
+
+	var awesompleter;
+	var dummyInput;
+
+	beforeEach(function(){
+		var dummyBody = document.createElement('body');
+		dummyInput = document.createElement('input');
+		dummyBody.appendChild(dummyInput);
+		awesompleter = new Awesomplete(dummyInput);
+	})
+
+	describe("default object",function(){
+		
+		it("should have an empty list",function(){
+			expect(awesompleter._list.length).toBe(0);
+		})
+
+		it("should have 2 min chars",function(){
+			expect(awesompleter.minChars).toBe(2);
+		})
+
+		it("should have 10 max items",function(){
+			expect(awesompleter.maxItems).toBe(10);
+		})	
+	})
+
+	describe("custom object",function(){
+		it("should have a non-empty list",function(){
+			awesompleter.list = ["Prolog"];
+			expect(awesompleter._list.length).toBe(1);
+		})
+
+		it("should have 3 min chars",function(){
+			awesompleter = new Awesomplete(dummyInput,{minChars:3});
+			expect(awesompleter.minChars).toBe(3);
+		})
+	})
+	
+});
+
 describe("awesomplete helpers",function(){
 
 	describe("$ function",function(){
