bb8f148e8b620d60b6c8ccd2f3cdd35ec17c016a
Vladislav Zarakovsky <vlad.zar@gmail.com>
2015-11-23 18:55:49 +0300

    Add karma-jasmine-def for def() and subject() test helpers

--- karma.conf.js
+++ karma.conf.js
@@ -5,7 +5,7 @@ module.exports = function(config) {
 
 		// frameworks to use
 		// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-		frameworks: ['jasmine'],
+		frameworks: ['jasmine', 'jasmine-def'],
 
 		// list of files / patterns to load in the browser
 		files: [
