1aa72f7c873cf2e439757808c90af7acb057781a
Vladislav Zarakovsky <vlad.zar@gmail.com>
2015-11-23 19:00:18 +0300

    Add karma-fixture for loading fixtures from files

--- karma.conf.js
+++ karma.conf.js
@@ -5,12 +5,16 @@ module.exports = function(config) {
 
 		// frameworks to use
 		// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-		frameworks: ['jasmine', 'jasmine-def'],
+		frameworks: ['jasmine', 'jasmine-def', 'fixture'],
 
 		// list of files / patterns to load in the browser
 		files: [
 			'awesomplete.js',
-      'test/**/*Shared.js',
+			{
+				pattern: 'test/fixtures/**/*.html',
+				watched: true, included: true, served: true
+			},
+			'test/**/*Shared.js',
 			'test/**/*Spec.js'
 		],
 
@@ -22,6 +26,7 @@ module.exports = function(config) {
 		// preprocess matching files before serving them to the browser
 		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
 		preprocessors: {
+			'**/*.html' : ['html2js']
 		},
 
 		// test results reporter to use
