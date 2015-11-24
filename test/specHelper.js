eff8334482cb0915980a0c543218009af0c3e682
Vladislav Zarakovsky <vlad.zar@gmail.com>
2015-11-24 15:26:12 +0300

    Constructor options tests

--- test/specHelper.js
+++ test/specHelper.js
@@ -1,1 +1,24 @@
 fixture.setBase("test/fixtures");
+
+// finds DOM elements in tests
+function $ (str, context) {
+	return (context || fixture.el).querySelector(str);
+}
+
+// bundled fixture load/cleanup
+$.fixture = function (fixtureName) {
+	beforeEach(function () {
+		fixture.load(fixtureName + ".html");
+	});
+
+	afterEach(function () {
+		fixture.cleanup();
+	});
+}
+
+// $.noop returns a new empty function each time it's being called
+Object.defineProperty($, "noop", {
+	get: function () {
+		return function noop () {}
+	}
+});
