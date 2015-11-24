94bb12f82be2ffa6c9adae153b8880cbbae77dfe
Vladislav Zarakovsky <vlad.zar@gmail.com>
2015-11-24 14:18:35 +0300

    mousedown event tests

---
+++ test/events/mousedownSpec.js
@@ -0,0 +1,45 @@
+describe("mousedown event", function () {
+
+	$.fixture("plain");
+
+	subject(function () {
+		return new Awesomplete("#plain", { list: ["item1", "item2", "item3"] });
+	});
+
+	beforeEach(function () {
+		this.subject.input.focus();
+		this.subject.open();
+		$.type(this.subject.input, "ite");
+		this.subject.next();
+	});
+
+	describe("with ul target", function () {
+		it("does not select item", function () {
+			spyOn(this.subject, "select");
+			$.fire(this.subject.ul, "mousedown");
+
+			expect(this.subject.select).not.toHaveBeenCalled();
+		});
+	});
+
+	describe("with li target", function () {
+		it("selects item", function () {
+			var li = this.subject.ul.children[1];
+			spyOn(this.subject, "select");
+			$.fire(li, "mousedown");
+
+			expect(this.subject.select).toHaveBeenCalledWith(li);
+		});
+	});
+
+	describe("with child of li target", function () {
+
+		it("selects item", function () {
+			var li = this.subject.ul.children[1];
+			spyOn(this.subject, "select");
+			$.fire($("mark", li), "mousedown");
+
+			expect(this.subject.select).toHaveBeenCalledWith(li);
+		});
+	});
+});
