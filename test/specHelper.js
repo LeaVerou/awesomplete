4fc68f6302d9606406d88e28ab9181f01779bbb4
Vladislav Zarakovsky <vlad.zar@gmail.com>
2015-11-24 13:42:08 +0300

    awesomplete.next tests

--- test/specHelper.js
+++ test/specHelper.js
@@ -26,6 +26,22 @@ $.on = function (element, event, callback) {
 	element.addEventListener(event, callback);
 };
 
+$.fire = function (target, type, properties) {
+	var evt = document.createEvent("HTMLEvents");
+	evt.initEvent(type, true, true );
+	for (var j in properties) {
+		evt[j] = properties[j];
+	}
+	target.dispatchEvent(evt);
+};
+
+// simulates text input (very simple, only "input" event is fired)
+$.type = function (input, text) {
+	input.focus();
+	input.value = text;
+	$.fire(input, "input");
+}
+
 // $.noop returns a new empty function each time it's being called
 Object.defineProperty($, "noop", {
 	get: function () {
