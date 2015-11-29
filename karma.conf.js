6c88a3e3c27d26f8e4d02f40ab54028a6a776ca0
Vladislav Zarakovsky <vlad.zar@gmail.com>
2015-11-29 19:27:35 +0300

    Generate code coverage

--- karma.conf.js
+++ karma.conf.js
@@ -22,13 +22,17 @@ module.exports = function(config) {
 		// preprocess matching files before serving them to the browser
 		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
 		preprocessors: {
+			'awesomplete.js': ['coverage']
 		},
 
 		// test results reporter to use
 		// possible values: 'dots', 'progress'
 		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
-		reporters: ['progress'],
-
+		reporters: ['progress', 'coverage'],
+		coverageReporter: {
+			type: 'lcov',
+			subdir: '.'
+		},
 		// web server port
 		port: 9876,
 
