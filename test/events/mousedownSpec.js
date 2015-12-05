f9d6cca542c66aef3c550e65cef3940393dab88e
Vladislav Zarakovsky <vlad.zar@gmail.com>
2015-12-05 17:47:52 +0300

    Tests for merged #16791 Don't select items on mouse right-click

--- test/events/mousedownSpec.js
+++ test/events/mousedownSpec.js
@@ -11,34 +11,45 @@ describe("mousedown event", function () {
 		this.subject.open();
 		$.type(this.subject.input, "ite");
 		this.subject.next();
+
+		spyOn(this.subject, "select");
 	});
 
-	describe("with ul target", function () {
-		it("does not select item", function () {
-			spyOn(this.subject, "select");
-			$.fire(this.subject.ul, "mousedown");
+	def("li", function () { return this.subject.ul.children[1] });
 
+	describe("with ul target", function () {
+		def("target", function () { return this.subject.ul });
+
+		it("does not select item", function () {
+			$.fire(this.target, "mousedown", { button: 0 });
 			expect(this.subject.select).not.toHaveBeenCalled();
 		});
 	});
 
 	describe("with li target", function () {
-		it("selects item", function () {
-			var li = this.subject.ul.children[1];
-			spyOn(this.subject, "select");
-			$.fire(li, "mousedown");
+		def("target", function () { return this.li });
 
-			expect(this.subject.select).toHaveBeenCalledWith(li);
+		describe("on left click", function () {
+			it("selects item", function () {
+				$.fire(this.target, "mousedown", { button: 0 });
+				expect(this.subject.select).toHaveBeenCalledWith(this.li);
+			});
+		});
+
+		describe("on right click", function () {
+			it("does not select item", function () {
+				$.fire(this.target, "mousedown", { button: 2 });
+				expect(this.subject.select).not.toHaveBeenCalled();
+			});
 		});
 	});
 
 	describe("with child of li target", function () {
-		it("selects item", function () {
-			var li = this.subject.ul.children[1];
-			spyOn(this.subject, "select");
-			$.fire($("mark", li), "mousedown");
+		def("target", function () { return $("mark", this.li) });
 
-			expect(this.subject.select).toHaveBeenCalledWith(li);
+		it("selects item", function () {
+			$.fire(this.target, "mousedown", { button: 0 });
+			expect(this.subject.select).toHaveBeenCalledWith(this.li);
 		});
 	});
 });
