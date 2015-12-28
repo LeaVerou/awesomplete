c505ab7b0a466f0aab57d1d2460504cbc0726cd1
Vladislav Zarakovsky <vlad.zar@gmail.com>
2015-12-28 09:34:08 +0300

    Post merge fix

--- karma.conf.js
+++ karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function(config) {
 		// preprocess matching files before serving them to the browser
 		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
 		preprocessors: {
-			'awesomplete.js': ['coverage']
+			'awesomplete.js': ['coverage'],
 			'**/*.html' : ['html2js']
 		},
 
