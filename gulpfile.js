ed99eae60cfefd09fab38f73ef18b4fbb873253e
Marco Biedermann <marco.biedermann@gmx.de>
2016-09-08 21:27:34 +0200

    Feature: Sourcemaps (#16963)
    * add "gulp-sourcemaps" to create a CSS sourcemap
    
    * replace "gulp-closure-compiler-service" with "gulp-uglify"

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,28 @@
 var gulp = require('gulp');
-var closure = require('gulp-closure-compiler-service');
+var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var header = require('gulp-header');
 var concat = require('gulp-concat');
+var sourcemaps = require('gulp-sourcemaps');
 
 var banner = "// Awesomplete - Lea Verou - MIT license\n";
 
 gulp.task('minify', function() {
 	return gulp.src(['awesomplete.js'])
-		.pipe(closure({
-			compilation_level: 'SIMPLE_OPTIMIZATIONS',
-			language: 'ECMASCRIPT5'
-		}))
+		.pipe(sourcemaps.init())
+		.pipe(uglify())
 		.pipe(rename({ suffix: '.min' }))
 		.pipe(header(banner))
+		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest('.'));
 });
 
 gulp.task('concat', function() {
 
 	return gulp.src(['awesomplete.base.css', 'awesomplete.theme.css'])
+		.pipe(sourcemaps.init())
 		.pipe(concat('awesomplete.css'))
+		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest('.'));
 
 
